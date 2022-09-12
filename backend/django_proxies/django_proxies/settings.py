e1e52439d197365c1cc12a3ef24d135fb103f72a
Matthew Dinh <32109652+MatthewDinh419@users.noreply.github.com>
2022-09-12 09:08:04 -0600

    Update settings.py
    Removed secret key and instead uses environmental variables

--- backend/django_proxies/django_proxies/settings.py
+++ backend/django_proxies/django_proxies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9lna0^s=8ie0exl%q5$xgf9n5s2_cj1slknozaam-u(j37l=wq'
+os.environ.get('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
